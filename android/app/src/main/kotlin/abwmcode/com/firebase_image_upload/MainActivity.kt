a8e7b2e216264eab50bfc8631c0158367ce592a9
The-made <abwmcode@gmail.com>
2024-06-12 06:45:38 +0100

    ref:upload

---
+++ android/app/src/main/kotlin/abwmcode/com/firebase_image_upload/MainActivity.kt
@@ -0,0 +1,6 @@
+package abwmcode.com.firebase_image_upload
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/login.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({Key? key}) : super(key: key);
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: ElevatedButton(
+            onPressed: () {
+              signInWithGoogle();
+            },
+            child: Text('Login with Google')),
+      ),
+    );
+  }
+
+  signInWithGoogle() async {
+    GoogleSignInAccount? googleUser = await GoogleSignIn().signIn();
+    GoogleSignInAuthentication? googleAuth = await googleUser?.authentication;
+    AuthCredential credential = GoogleAuthProvider.credential(
+      accessToken: googleAuth?.accessToken,
+      idToken: googleAuth?.idToken,
+    );
+    UserCredential userCredential =
+        await FirebaseAuth.instance.signInWithCredential(credential);
+    print(userCredential.user?.displayName);
+  }
+}
